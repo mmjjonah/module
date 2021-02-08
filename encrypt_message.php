26edf32983f0058be3ad3c77bf6b59bf31bbcf23
mmjjonah <mmjjonah@gmail.com>
2021-02-08 09:28:53 +0300

    cryptage de message PHP

---
+++ encrypt_message.php
@@ -0,0 +1,20 @@
+// encryption function
+function simple_crypt( $string, $action = 'e' ) {
+    // you may change these values to your own
+    $secret_key = AUTH_SALT;
+    $secret_iv = LOGGED_IN_SALT;
+ 
+    $output = false;
+    $encrypt_method = "AES-256-CBC";
+    $key = hash( 'sha256', $secret_key );
+    $iv = substr( hash( 'sha256', $secret_iv ), 0, 16 );
+ 
+    if( $action == 'e' ) {
+        $output = base64_encode( openssl_encrypt( $string, $encrypt_method, $key, 0, $iv ) );
+    }
+    else if( $action == 'd' ){
+        $output = openssl_decrypt( base64_decode( $string ), $encrypt_method, $key, 0, $iv );
+    }
+ 
+    return $output;
+}
